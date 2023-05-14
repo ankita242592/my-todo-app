c271cded54611e5b76eb57be2b59af62af0eb232
ankita242592 <ankitapatil242592@gmail.com>
2023-05-14 16:55:21 +0530

    initial commit

---
+++ functions.py
@@ -0,0 +1,15 @@
+FILEPATH = r'C:\Users\ankit\PycharmProjects\Todoapp\todos.txt'
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath,'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_args,filepath=FILEPATH):
+    with open(filepath,'w') as file_local:
+        file_local.writelines(todos_args)
+
+if __name__ =="__main__":
+    print("hello")
+    print(get_todos())
